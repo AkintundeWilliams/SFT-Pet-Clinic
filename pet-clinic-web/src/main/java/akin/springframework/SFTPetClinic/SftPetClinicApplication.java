0f00173d9660721cfb3556849194fb2ed474f7df
Akintunde.Williams <Akintunde.Williams@fdmgroup.com>
2022-12-30 00:23:21 +0100

    including modifications on files

---
+++ pet-clinic-web/src/main/java/akin/springframework/SFTPetClinic/SftPetClinicApplication.java
@@ -0,0 +1,11 @@
+package akin.springframework.SFTPetClinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SftPetClinicApplication {
+
+	public static void main(String[] args) {SpringApplication.run(SftPetClinicApplication.class, args);}
+
+}
---
+++ src/test/java/akin/springframework/SFTPetClinic/SftPetClinicApplicationTests.java
@@ -0,0 +1,14 @@
+package akin.springframework.SFTPetClinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class SftPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
