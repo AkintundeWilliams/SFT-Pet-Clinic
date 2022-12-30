5a36bf557e51b12c9e6e84c3437c8c5416691e34
Akintunde.Williams <Akintunde.Williams@fdmgroup.com>
2022-12-30 12:54:19 +0100

    adding base entity. closes #10

--- pet-clinic-data/src/main/java/akin/springframework/SFTPetClinic/model/Person.java
+++ pet-clinic-data/src/main/java/akin/springframework/SFTPetClinic/model/Person.java
@@ -1,6 +1,6 @@
 package akin.springframework.SFTPetClinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/akin/springframework/SFTPetClinic/model/PetType.java
+++ pet-clinic-data/src/main/java/akin/springframework/SFTPetClinic/model/PetType.java
@@ -1,6 +1,6 @@
 package akin.springframework.SFTPetClinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
     private String name;
 
     public String getName() {
